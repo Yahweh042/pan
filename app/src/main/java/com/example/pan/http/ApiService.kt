5eaf9e810bf37bb51a589c40c82a7b4f716a3982
xuyi2 <xuyi2@liepin.com>
2021-03-12 19:57:17 +0800

    init

--- app/src/main/java/com/example/pan/http/ApiService.kt
+++ app/src/main/java/com/example/pan/http/ApiService.kt
@@ -7,12 +7,21 @@ import retrofit2.http.*
 
 interface ApiService {
 
+    /**
+     * 获取用户信息
+     */
     @GET("/rest/2.0/xpan/nas?method=uinfo")
     suspend fun getUserInfo(): UserInfo
 
+    /**
+     * 获取网盘容量
+     */
     @GET("/api/quota")
     suspend fun quota(): Quota
 
+    /**
+     * 获取文件列表
+     */
     @GET("/rest/2.0/xpan/file?method=list")
     suspend fun list(
         @Query("dir") dir: String,
@@ -22,19 +31,31 @@ interface ApiService {
         @Query("limit") num: Int,
     ): ResponseData<FileInfo>
 
+    /**
+     * 获取文件信息
+     */
     @GET("/rest/2.0/xpan/multimedia?method=filemetas&dlink=1")
     suspend fun filemetas(
         @Query("fsids") fsids: String
     ): ResponseData<FileMeta>
 
+    /**
+     * 获取已下载完成列表
+     */
     @Headers("baseUrl: aria2")
     @POST("/")
     suspend fun tellStopped(@Body body: RequestBody): Aria2Response<List<Aria2TaskInfo>>
 
+    /**
+     * 获取正在下载完成列表
+     */
     @Headers("baseUrl: aria2")
     @POST("/")
     suspend fun tellActive(@Body body: RequestBody): Aria2Response<List<Aria2TaskInfo>>
 
+    /**
+     * 添加Url
+     */
     @Headers("baseUrl: aria2")
     @POST("/")
     suspend fun addUri(@Body body: RequestBody): Aria2Response<String>
